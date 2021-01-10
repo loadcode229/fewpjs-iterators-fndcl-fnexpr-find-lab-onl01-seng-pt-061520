1e35668db356d708f01cb90c67b552ca3e9ed947
Your Name <you@example.com>
2021-01-10 17:37:18 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = (record) => {
+  const preferredResult = record.find(({ result }) => result === "W");
+  return (preferredResult ? preferredResult.year : undefined)
+}
